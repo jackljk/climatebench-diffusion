b88b97c166fca50b130c3869e3ff05448bf0427b
Jackljk <94715891+jackljk@users.noreply.github.com>
2025-01-29 02:26:17 -0800

    Fix data downloader

--- download_data/download.py
+++ download_data/download.py
@@ -8,11 +8,12 @@ from src.utilities.s3utils import download_s3_object
 local_data_dir = "/data"
 # ---------------------------------------
 files = [
-    "outputs_ssp126_daily.nc",
-    "outputs_ssp245_daily.nc",
-    "outputs_ssp370_daily.nc",
-    "outputs_ssp585_daily.nc",
-    "outputs_historical_daily.nc",
+    # "outputs_ssp126_daily.nc",
+    # "outputs_ssp245_daily.nc",
+    # "outputs_ssp370_daily.nc",
+    # "outputs_ssp585_daily.nc",
+    "outputs_historical_daily_raw.nc",
+    "CESM2_piControl_r1i1p1f1_climatology_daily.nc",
     "inputs_ssp126.nc",
     "inputs_ssp245.nc",
     "inputs_ssp370.nc",
