415d6d8e85d9692bafdf365bf4f8054e54216488
Jackljk <94715891+jackljk@users.noreply.github.com>
2025-01-30 17:51:12 -0800

    update shell script for edm-no-hist

--- scripts/edm-no-hist.sh
+++ scripts/edm-no-hist.sh
@@ -8,25 +8,22 @@ export SCRIPT_NAME=$(basename "$0")
 python run.py "$@" \
     experiment=climatebench_daily_edm \
     datamodule=climatebench_daily \
-    trainer=default \
-    trainer.devices=2 \
-    trainer.max_epochs=100 \
-    datamodule.data_dir=/data/normalized \
+    datamodule.data_dir=/data/raw \ 
     datamodule.mean_over_ensemble='first' \
     datamodule.simulations=["ssp126","ssp370","ssp585"] \
-    module.num_predictions=10 module.prediction_inputs_noise=0.0 \
-    module.enable_inference_dropout=True \
-    module.optimizer.lr=1e-4 module.optimizer.weight_decay=1e-4 \
     datamodule.window=1 \
     datamodule.batch_size=128 \
     datamodule.eval_batch_size=8 \
     datamodule.batch_size_per_gpu=16 \
+    datamodule.simulations_anom_type="none" \
+    datamodule.simulations_raw="all" \
+    datamodule.normalization_type="standard" \
+    trainer.devices=2 \
+    trainer.max_epochs=100 \
     model.dim=128 \
-    callbacks.early_stopping.patience=10 test_after_training=True \
-    datamodule.DEBUG_dataset_size=null \
-    suffix=edm_batch_128 \
+    module.num_predictions=10 module.prediction_inputs_noise=0.0 \
+    callbacks.early_stopping.patience=20 test_after_training=True \
+    suffix=edm_no_hist \
     "$@"
-
-
 # File to run a base EDM model
 # Already has Historical data used during training (in config file)
