039764e50c83861e40d5e0d81e3b9994e3e1ea2c
Jackljk <94715891+jackljk@users.noreply.github.com>
2025-02-04 11:04:53 -0800

    test 1

--- scripts/edm-no-hist.sh
+++ scripts/edm-no-hist.sh
@@ -13,15 +13,16 @@ python run.py "$@" \
     datamodule.simulations=["ssp126","ssp370","ssp585"] \
     datamodule.window=1 \
     datamodule.batch_size=128 \
-    datamodule.eval_batch_size=8 \
-    datamodule.batch_size_per_gpu=16 \
+    datamodule.eval_batch_size=3 \
+    datamodule.batch_size_per_gpu=6 \
     datamodule.simulations_anom_type="none" \
     datamodule.simulations_raw="all" \
     datamodule.normalization_type="standard" \
-    trainer.devices=2 \
+    trainer=ddp \
+    trainer.devices=8 \
     trainer.max_epochs=100 \
     model.dim=128 \
-    module.num_predictions=10 module.prediction_inputs_noise=0.0 \
+    module.num_predictions=5 module.prediction_inputs_noise=0.0 \
     callbacks.early_stopping.patience=20 test_after_training=True \
     suffix=edm_no_hist \
     "$@"
