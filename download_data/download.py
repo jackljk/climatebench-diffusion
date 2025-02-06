44b9426cfc69f8447307edf1986bf6acf2848d9f
Jackljk <94715891+jackljk@users.noreply.github.com>
2025-02-05 16:46:54 -0800

    updated download.py

--- download_data/download.py
+++ download_data/download.py
@@ -5,28 +5,35 @@ os.environ["S3_BUCKET_NAME"] = "climatebench"
 from src.utilities.s3utils import download_s3_object
 
 # -------------- Edit this to the directory where you'd like the data to be in --------------
-local_data_dir = "/data/raw"
+local_data_dir = "/data"
 # ---------------------------------------
 files = [
-    # "outputs_ssp126_daily.nc",
-    # "outputs_ssp245_daily.nc",
-    # "outputs_ssp370_daily.nc",
-    # "outputs_ssp585_daily.nc",
-    # "CESM2_piControl_r1i1p1f1_climatology_daily.nc",
-    # "inputs_ssp126.nc",
-    # "inputs_ssp245.nc",
-    # "inputs_ssp370.nc",
-    # "inputs_ssp585.nc",
-    # "inputs_historical.nc",
-    # "CESM2-rsdt-Amon-gn-ssp126.nc",
-    # "CESM2-rsdt-Amon-gn-historical.nc",
-    # "CESM2-rsdt-Amon-gn-piControl.nc",
-    # "outputs_ssp126_daily_raw.nc",
+    # Outputs daily
+    "outputs_ssp126_daily.nc",
+    "outputs_ssp245_daily.nc",
+    "outputs_ssp370_daily.nc",
+    "outputs_ssp585_daily.nc",
+    # PI Control
+    "CESM2_piControl_r1i1p1f1_climatology_daily.nc",
+    # Inputs
+    "inputs_ssp126.nc",
+    "inputs_ssp245.nc",
+    "inputs_ssp370.nc",
+    "inputs_ssp585.nc",
+    "inputs_historical.nc",
+    # RSDT
+    "CESM2-rsdt-Amon-gn-ssp126.nc",
+    "CESM2-rsdt-Amon-gn-historical.nc",
+    "CESM2-rsdt-Amon-gn-piControl.nc",
+    # Raw data daily
+    "outputs_ssp126_daily_raw.nc",
     "outputs_ssp245_daily_raw.nc",
     "outputs_ssp370_daily_raw.nc",
     "outputs_ssp585_daily_raw.nc",
-    # "outputs_historical_daily_raw.nc",
+    "outputs_historical_daily_raw.nc",
 ]
+
+
 s3_data_dir = "data/"
 
 # Ensure local data directory exists
