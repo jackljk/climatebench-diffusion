4d39feea05cfea94dd182db7c195828fc3df65fa
Jack Kai Lim <limjackailjk@gmail.com>
2025-05-22 23:23:44 -0700

    update scripts

--- scripts/current_best_script/day-tas-pr-edm-adm-pcw-std-wmse-rsdt-44lr-pm-1-all-ens-d64.sh
+++ scripts/current_best_script/day-tas-pr-edm-adm-pcw-std-wmse-rsdt-44lr-pm-1-all-ens-d64.sh
@@ -27,7 +27,7 @@ python run.py "$@" \
     diffusion.loss_function="wmse" diffusion.P_mean=-1 diffusion.P_std=1.2 \
     diffusion.sigma_max_inf=400 diffusion.sigma_min=0.02 diffusion.num_steps=16 \
     datamodule.DEBUG_dataset_size=null \
-    suffix="raw_stdized2-Pm-1Ps1.2-ebs512" \
+    suffix="raw_stdized2-Pm-1Ps1.2-ebs512+rsdt_stdized" \
     "$@"
 
 #     datamodule.batch_size=256 is too low
